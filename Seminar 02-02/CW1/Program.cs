82ab3bf9263697f0f204a5bc6e65fd21a300790b
Dmitriy Lisitsyn <lisitsin1998@mail.ru>
2023-02-05 01:36:22 +0300

    Homework 30-01

---
+++ Seminar 02-02/CW1/Program.cs
@@ -0,0 +1,24 @@
+﻿System.Console.Write ("Введите число X: ");
+
+int numX = int.Parse (Console.ReadLine());
+
+Console.Write ("Введите число Y: ");
+
+int numY = int.Parse (Console.ReadLine());
+
+if (numX > 0 && numY > 0)
+{
+    System.Console.WriteLine ("1 секция");
+}
+else if (numX < 0 && numY > 0)
+{
+    System.Console.WriteLine ("2 секция");
+}
+else if (numX < 0 && numY < 0)
+{
+    System.Console.WriteLine ("3 секция");
+}
+else if (numX > 0 && numY < 0)
+{
+    System.Console.WriteLine ("4 секция");
+}
---
+++ Seminar 02-02/CW2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write ("Enter a number: ");
+int num = int.Parse(Console.ReadLine());
+int x = 1;
+while (x <= num)
+{
+    Console.Write (x*x+ " ");
+    x += 1;
+}
