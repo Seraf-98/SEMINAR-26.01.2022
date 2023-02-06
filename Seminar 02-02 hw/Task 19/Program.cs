572c0f4e26bf35d4f603614885af9db452435dd3
Dmitriy Lisitsyn <lisitsin1998@mail.ru>
2023-02-06 20:14:03 +0300

    fix

--- Seminar 02-02 hw/Task 19/Program.cs
+++ Seminar 02-02 hw/Task 19/Program.cs
@@ -15,7 +15,7 @@ int number = int.Parse(Console.ReadLine());
 
 if (number>10000 && number<100000) 
 {
-    if (number/10000 == number%10 || (number/1000)%10 == (number%100)/10)
+    if (number/10000 == number%10 && (number/1000)%10 == (number%100)/10)
     {
         Console.WriteLine(number+ " является палиндромом");
     }
