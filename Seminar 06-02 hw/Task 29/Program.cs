28de7641ebdced8222e047388b30be031811a92e
Dmitriy Lisitsyn <lisitsin1998@mail.ru>
2023-02-11 17:31:54 +0300

    hw task fix

---
+++ Seminar 06-02 hw/Task 29/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int [] array = GetArray (8, -10, 10);
+
+Console.WriteLine(string.Join(" ", array));
+
+int [] GetArray (int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue +1);
+    }
+    return res;
+}
+
