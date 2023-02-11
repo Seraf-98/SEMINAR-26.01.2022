8dde7c580fc5bc73a1f2d87b333d1fad6cdec3b9
Dmitriy Lisitsyn <lisitsin1998@mail.ru>
2023-02-11 17:32:08 +0300

    hw task fix

---
+++ Seminar 06-02 hw/Task 27/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12
+
+*/
+
+int enterNum (string message)
+{
+    Console.Write(message);
+    string input = Console.ReadLine();
+    int result = int.Parse(input);
+    return result;
+}
+
+int Sum (int Number)
+{
+    int res1 = 0;
+    while (Number > 0)
+    {
+        res1 += Number % 10;
+        Number = Number / 10;
+    }
+    return res1;
+}
+
+int Number = enterNum("Введите число: ");
+Console.Write($"Ответ: {Sum(Number)}");
