6cf03ee2b92445af7b3fb71b81899af1c3361980
Dmitriy Lisitsyn <lisitsin1998@mail.ru>
2023-02-13 22:04:12 +0300

    cw task complite

---
+++ Seminar 13-02 cw/Task 2/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Введите координату для точки А");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату для точки В");
+int B = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату для точки С");
+int C = int.Parse(Console.ReadLine());
+
+bool IsTriangle(int A, int B, int C)
+{
+    return (((A+B)>C)&&((A+C)>B)&&((B+C)>A));
+}
+
+if (IsTriangle(A, B, C))
+{
+    Console.WriteLine("Может существовать");
+}
+else
+{
+    Console.WriteLine("Не может существовать");
+}
