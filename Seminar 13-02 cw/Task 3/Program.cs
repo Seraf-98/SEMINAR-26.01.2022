96144ad34fff5974146cbe548bf941bbf77b3bfa
Dmitriy Lisitsyn <lisitsin1998@mail.ru>
2023-02-13 22:04:01 +0300

    cw task complite

---
+++ Seminar 13-02 cw/Task 3/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] NumbersConvert(int num)
+{
+    int size = 32;
+    int[] Result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Result[i] = num %2;
+        num /=2;
+
+    }
+    return Result;
+}
+
+int[] Binary = (NumbersConvert(number));
+Array.Reverse(Binary);
+Console.WriteLine($"Полученный массив = "+ String.Join(", ", Binary));
