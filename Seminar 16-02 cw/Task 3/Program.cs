ad5e02a735cffa1f65faf0a13da76ad03fba9a7f
Dmitriy Lisitsyn <lisitsin1998@mail.ru>
2023-02-16 22:18:23 +0300

    cw task complite

---
+++ Seminar 16-02 cw/Task 3/Program.cs
@@ -0,0 +1,80 @@
+﻿
+int numRows = SetNumber("Rows");
+
+int numColumns = SetNumber("Columns");
+
+int numMaxValue = SetNumber("Max Value");
+
+int numMinValue = SetNumber("Min Value");
+
+var matrix = GetMatrix(numRows, numColumns, numMinValue, numMaxValue);
+Print(matrix);
+Console.WriteLine(Sum(matrix));
+
+int SetNumber(string numberName)
+{
+    Console.Write($"Enter number {numberName}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+
+int[,] GetMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int l = 0; l < columns; l++)
+        {
+            matrix[i, l] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void Print(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            System.Console.Write(matrix[i, l] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void CheckValue(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            if (IsEven(i,l));
+            {
+                matrix[i,l] *= matrix[i,l];
+            }
+        }
+    }
+}
+
+bool IsEven(int i, int l)
+{
+    return i == l;
+}
+
+int Sum(int[,] matrix)
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            if (IsEven(i,l))
+            {
+                sum += matrix[i,l];
+            }   
+        }
+    }
+     return sum;
+}
