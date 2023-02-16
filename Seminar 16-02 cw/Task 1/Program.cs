f8b196407dee4e0f35668a9113eb36b64408d435
Dmitriy Lisitsyn <lisitsin1998@mail.ru>
2023-02-16 22:17:30 +0300

    cw task complite

---
+++ Seminar 16-02 cw/Task 1/Program.cs
@@ -0,0 +1,40 @@
+﻿
+int numRows = SetNumber("Rows");
+
+int numColumns = SetNumber("Columns");
+
+var matrix = GetMatrix(numRows, numColumns);
+Print(matrix);
+
+int SetNumber(string numberName)
+{
+    Console.Write($"Enter number {numberName}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+
+int[,] GetMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int l = 0; l < columns; l++)
+        {
+            matrix[i, l] = i + l;
+        }
+    }
+    return matrix;
+}
+
+void Print(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            System.Console.Write(matrix[i, l] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
