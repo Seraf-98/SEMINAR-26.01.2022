20fd56214e2e6ebd796084be6720f08e0b68a4bf
Dmitriy Lisitsyn <lisitsin1998@mail.ru>
2023-02-19 20:37:09 +0300

    hw task complite

---
+++ Seminar 13-02 hw/Task 41/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+int lenght = enterNum("Введите количество элементов: ");
+
+int[] array;
+
+array = InputArray(lenght);
+
+Console.WriteLine($"Количество чисел больше 0 = {CountPositiveNumbers(array)}");
+
+int enterNum (string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int[] InputArray(int length)
+{
+    int[] array = new int[length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = enterNum($"Введите {i + 1}-й элемент: ");
+    }
+    return array;
+}
+
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
