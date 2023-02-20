285c9a657e039ecc5d2da71e2858c1fa9a6d2ab8
Dmitriy Lisitsyn <lisitsin1998@mail.ru>
2023-02-20 22:27:12 +0300

    cw task complite

---
+++ Seminar 20-02 cw/Task 2/Program.cs
@@ -0,0 +1,66 @@
+﻿// Программа заменяющая строки на столбцы в двумерном массиве
+
+int[,] GetMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int l = 0; l < columns; l++)
+        {
+            matrix[i, l] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            System.Console.Write(matrix[i, l] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void RowReverse(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        int temp = matrix[0, i];
+        matrix[0, i] = matrix[matrix.GetLength(0) - 1, i];
+        matrix[matrix.GetLength(0) - 1, i] = temp;
+    }
+}
+
+void RowsAndColumnsReverse(int[,] matrix)
+{
+    int temp;
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = i + 1; j < matrix.GetLength(1); j++)
+        {
+            temp = matrix[i, j];
+            matrix[i, j] = matrix[j, i];
+            matrix[j, i] = temp;
+        }
+    }
+}
+
+var matrix = GetMatrix(4, 4, 1, 10);
+
+while (true)
+{
+    if (matrix.GetLength(0) == matrix.GetLength(1))
+        break;
+    else Console.Write("Некорректный ввод, повторите.");
+}
+
+PrintMatrix(matrix);
+RowsAndColumnsReverse(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
