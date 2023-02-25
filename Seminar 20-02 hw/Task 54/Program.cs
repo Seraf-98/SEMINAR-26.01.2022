455a1fced911f3a00b5ad0ecb018aa90b563173d
Dmitriy Lisitsyn <lisitsin1998@mail.ru>
2023-02-25 23:54:18 +0300

    hw task complite

---
+++ Seminar 20-02 hw/Task 54/Program.cs
@@ -0,0 +1,82 @@
+﻿/*
+Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+Console.Clear();
+
+int numRows = SetNumber("количество строк");
+
+int numColumns = SetNumber("количество столбцов");
+
+int numMinValue = SetNumber("минимальное значение");
+
+int numMaxValue = SetNumber("максимальное значение");
+
+var matrix = GetMatrix(numRows, numColumns, numMinValue, numMaxValue);
+Print(matrix);
+SortRows(matrix);
+Console.WriteLine();
+Print(matrix);
+Console.WriteLine();
+
+int SetNumber(string numberName)
+{
+    Console.Write($"Введите {numberName}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+
+int[,] GetMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int l = 0; l < columns; l++)
+        {
+            matrix[i, l] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void Print(int[,] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            System.Console.Write(matrix[i, l] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void SortRows(int[,] matrix)  // метод сортировки строк по убыванию
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int k = 0; k < matrix.GetLength(1)-1; k++)
+            {
+                if (matrix[i,k] < matrix [i,k+1])
+                {
+                    int temp = matrix [i,k+1];
+                    matrix [i,k+1] = matrix[i,k];
+                    matrix[i,k] = temp;
+                }
+            }
+            
+        }
+    }
+}
